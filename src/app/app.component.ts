db9dc527667f31d43ed6567322cc010e3c072e58
Eslam Bashry <eslamhussin600@gmail.com>
2024-10-20 14:23:58 +0300

    new files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent, MainLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
   title = 'cinabon';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { SingleProductComponent } from './single-product/single-product.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '',
+    component: MainLayoutComponent,
+    children:[
+          { path: 'home', component: HomeComponent },
+          { path: 'about', component: AboutComponent },
+          { path: 'blogs', component: BlogsComponent },
+          { path: 'contact', component: ContactComponent },
+          { path: 'products', component: ProductsComponent },
+          { path: 'single-product', component: SingleProductComponent },
+    ]
+   }
+
+        ]
+
+
+
+        
